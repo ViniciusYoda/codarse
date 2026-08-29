5d1fab77848af262fe0a78f8f344c985c5b53e09
Vinícius Yoda <vyoda4604@gmail.com>
2026-08-29 11:29:09 -0300

    up

---
+++ src/app/(default)/cursos/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "CodarSe - Todos os cursos"
+}
+
+export default async function PageCursos() {
+
+    return (
+        <main className="mt-8 flex justify-center">
+            <div className="w-full min-[880px]:max-w-[880px]">
+                
+            </div>
+        </main>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+
 import "./globals.css";
 
-const nunito = Nunito({ subsets: ["latin"]})
+
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CodarSe - Os melhores cursos de programação gratuitos",
   description: "Os melhores cursos de programação gratuitos com a melhor experiência de aprendizado e foco",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+interface IRootLayout extends Readonly<{ children: React.ReactNode }> { }
+
+export default function RootLayout({ children }: IRootLayout) {
   return (
-    <html lang="pt-BR">
-      <body className={nunito.className}>{children}</body>
+    <html lang="pt-Br">
+      <body className={nunito.className}>
+        {children}
+      </body>
     </html>
   );
 }
