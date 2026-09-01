a55549b17040d04b95d720a7e2b793985d0fe6c0
Vinícius Yoda <vyoda4604@gmail.com>
2026-09-01 19:17:07 -0300

    Section

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,8 +1,19 @@
-import { Header } from "@/components/header/Header";
+import { Metadata } from "next";
+
+import { Section } from "@/components/section/Section";
+
+export const metadata: Metadata = {
+  title: "CodarSe - Página inicial"
+};
 
 
 export default function PageHome() {
   return (
-    <Header />
+    <main className="mt-8 flex justify-center">
+      <div className="w-full min-[880px]:max-w-[880px] flex flex-col gap-4">
+
+        
+      </div>
+    </main>
   );
 }
