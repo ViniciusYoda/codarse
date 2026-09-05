7a2037e3f432b265626a3ad7490ba8673f63e0a1
Vinícius Yoda <vyoda4604@gmail.com>
2026-09-05 11:15:29 -0300

    plsyr

---
+++ src/components/player/playlist/PlayerPlaylist.tsx
@@ -0,0 +1,74 @@
+'use client';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { IPlayerClassGroupProps, PlayerClassGroup } from './components/PlayerClassGroup';
+import { LocalStorage } from '@/shared/services/local-storage';
+
+interface IPlayerPlaylistProps {
+    playingClassId: string;
+    playingCourseId: string;
+    classGroups: Pick<IPlayerClassGroupProps, 'classes' | 'title'>[];
+}
+
+export const PlayerPlaylist = ({ classGroups, playingClassId, playingCourseId }: IPlayerPlaylistProps) => {
+    const router = useRouter();
+
+    const [watchedContentIds, setWatchedContentIds] = useState<string[]>([]);
+    const [openedIndex, setOpenedIndex] = useState<number | undefined>(
+        classGroups.findIndex((classGroup) => classGroup.classes.some(classItem => classItem.classId === playingClassId))
+    );
+
+    useEffect(() => {
+        const watchedContent = LocalStorage.watchedContent.get(playingCourseId);
+        if (!watchedContent) return;
+
+        setWatchedContentIds(watchedContent);
+    }, [playingCourseId]);
+
+    const classGroupsWithDone = useMemo(() => {
+        return classGroups.map(classGroup => ({
+            ...classGroup,
+            classes: classGroup.classes.map(classItem => ({
+                ...classItem,
+                done: watchedContentIds.includes(classItem.classId),
+            })),
+        }));
+    }, [classGroups, watchedContentIds]);
+
+    const handleCheck = useCallback((classId: string) => {
+        const newWatchedContent = LocalStorage.watchedContent.toggle(playingCourseId, classId);
+        if (!newWatchedContent) return;
+
+        setWatchedContentIds(newWatchedContent);
+    }, [playingCourseId]);
+
+    return (
+        <div className='flex flex-col gap-2 h-full'>
+            <div className='flex flex-col p-4 bg-paper'>
+                <h3 className='text-lg font-bold'>
+                    Conteudo do curso
+                </h3>
+            </div>
+
+            <ol className='overflow-auto overflow-primary'>
+                {classGroupsWithDone.map((classGroup, index) => (
+                    <li key={classGroup.title}>
+                        <PlayerClassGroup
+                            {...classGroup}
+
+                            playingClassId={playingClassId}
+
+                            position={index + 1}
+                            open={openedIndex === index}
+                            onToggle={() => setOpenedIndex(openedIndex === index ? undefined : index)}
+
+                            onCheck={handleCheck}
+                            onPlay={(classId) => router.push(`/player/${playingCourseId}/${classId}`)}
+                        />
+                    </li>
+                ))}
+            </ol>
+        </div>
+    )
+}
---
+++ src/components/player/playlist/components/PlayerClass.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { MdCheckCircle, MdCircle, MdPlayCircleOutline } from "react-icons/md";
+
+export interface IPlayerClassProps {
+    done: boolean;
+    title: string;
+    playing: boolean;
+
+    onPlay: () => void;
+    onCheck: () => void;
+}
+
+export const PlayerClass = ({ title, playing, done, onCheck, onPlay }: IPlayerClassProps) => {
+
+    return (
+        <button className="flex gap-6 p-4 items-center" onClick={() => onPlay()}>
+            <div className="group" onClick={e => { e.stopPropagation(); onCheck(); }}>
+                {!done
+                    ? (
+                        <>
+                            <MdPlayCircleOutline
+                                size={24}
+                                className="min-w-6 group-hover:hidden"
+                            />
+                            <MdCircle
+                                size={24}
+                                className="min-w-6 hidden group-hover:block"
+                            />
+                        </>
+                    )
+                    : (
+                        <MdCheckCircle
+                            size={24}
+                            className="min-w-6 hidden group-hover:block"
+                        />
+                    )
+                }
+            </div>
+
+            <div className="flex flex-col gap-1 items-start">
+                <p
+                    data-done={done}
+                    className="line-clamp-2 text-start data-[done=true]:text-green-400"
+                >
+                    {title}
+                </p>
+
+                {playing && (
+                    <span className="px-2 py-1 bg-blue-400 rounded-full leading-4">
+                        Reproduzindo
+                    </span>
+                )}
+            </div>
+        </button>
+    )
+}
